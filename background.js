b8f8ea79c5bbdbd2fb84dd8474ffcaaa73e4cf68
itsbrex <9772694+itsbrex@users.noreply.github.com>
2022-12-15 20:41:07 -0800

    added user_agent + cf_clearance token (#2)
    * added user_agent + cf_clearance tokens

--- background.js
+++ background.js
@@ -1,18 +1,36 @@
-// Copy the session token cookie to the clipboard
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-	chrome.cookies.get({ url: tabs[0].url, name: '__Secure-next-auth.session-token' }, (cookie) => {
-		var area = document.createElement('textarea');
-		area.style.position = 'absolute';
-		area.style.border = '0';
-		area.style.padding = '0';
-		area.style.margin = '0';
-		area.style.height = '1px';
-		area.style.top = '-10px';
-		area.innerText = cookie.value;
-		document.body.appendChild(area, document.body.firstChild);
-		area.focus();
-		area.select();
-		document.execCommand('copy');
-		document.body.removeChild(area);
-	});
+// Get the user agent using navigator.userAgent and append to finalOutput
+var userAgent = navigator.userAgent;
+var finalOutput = 'USER_AGENT: \r\n' + userAgent + '\r\n\r\n';
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+	chrome.cookies.get(
+		{ url: tabs[0].url, name: '__Secure-next-auth.session-token' },
+		function (cookie) {
+			finalOutput += 'SESSION_TOKEN: \r\n' + cookie.value + '\r\n\r\n';
+			chrome.cookies.get({ url: tabs[0].url, name: 'cf_clearance' }, function (cookie) {
+				finalOutput += 'CF_CLEARANCE: \r\n' + cookie.value + '\r\n';
+				var area = document.createElement('textarea');
+				area.style.position = 'absolute';
+				area.style.border = '0';
+				area.style.padding = '0';
+				area.style.margin = '0';
+				area.style.height = '1px';
+				area.style.top = '-10px';
+				area.innerHTML = finalOutput;
+				document.body.appendChild(area, document.body.firstChild);
+
+				var range = document.createRange();
+				range.selectNodeContents(area);
+
+				var selection = window.getSelection();
+				if (selection !== null) {
+					// check if selection is not null just to be safe
+					selection.removeAllRanges();
+					selection.addRange(range);
+					document.execCommand('copy');
+					document.body.removeChild(area);
+				}
+			});
+		},
+	);
 });
